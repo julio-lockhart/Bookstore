5573c02b621ae5f6d316dc4c9561894b24651a30
Julio Lockhart <julio.lockhart16@gmail.com>
2017-11-10 16:41:39 -0500

    Added route to search by category

--- routes/search.js
+++ routes/search.js
@@ -6,16 +6,27 @@ const data = require('../data');
 const searchAPI = data.search;
 
 router.get("/", async(req, res) => {
-
     await searchAPI.searchForBooks("*")
         .then(result => res.json(result))
         .catch(error => res.status(500).json({
-            "error": "Error: " + error
+            "error": error
         }));
 });
 
-router.get("/search/:bookTitle", (req, res) => {
-    res.sendStatus(200);
+router.get("/search/:bookTitle", async(req, res) => {
+    await searchAPI.searchForBooks(req.params.bookTitle)
+        .then(result => res.json(result))
+        .catch(error => res.status(500).json({
+            "error": error
+        }));
+});
+
+router.get("/category/:category", async(req, res) => {
+    await searchAPI.searchByCategory(req.params.category)
+        .then(result => res.json(result))
+        .catch(error => res.status(500).json({
+            "error": error
+        }));
 });
 
 module.exports = router;
