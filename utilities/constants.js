ecb488997a53a71dfea63972e9891d496b166860
Julio Lockhart <julio.lockhart16@gmail.com>
2017-11-10 16:39:53 -0500

    New URL to search by category

--- utilities/constants.js
+++ utilities/constants.js
@@ -1,3 +1,4 @@
 module.exports.URLS = Object.freeze({
-    SEARCH_URL: "https://www.googleapis.com/books/v1/volumes?q="
+    SEARCH_URL: "https://www.googleapis.com/books/v1/volumes?q=",
+    CATEGORY_URL: "https://www.googleapis.com/books/v1/volumes?q=subject:"
 });
