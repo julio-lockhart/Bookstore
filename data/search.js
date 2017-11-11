c731d29f74bd5fdf4097aeede6dd5dd29b6d9a2d
Julio Lockhart <julio.lockhart16@gmail.com>
2017-11-11 16:54:34 -0500

    Added a function that will parse the data from Google's API, and extracts some of the information needed for the project. Still need to add the remaining data.

--- data/search.js
+++ data/search.js
@@ -9,7 +9,8 @@ const searchForBooks = async(bookTitle) => {
     try {
         const response = await axios.get(searchUrl);
         const data = response.data.items;
-        return data;
+        const formattedBookInfo = await extractBookInformation(data);
+        return formattedBookInfo;
     } catch (error) {
         throw error;
     }
@@ -27,6 +28,48 @@ const searchByCategory = async(category) => {
     } catch (error) {
         throw error;
     }
+};
+
+const extractBookInformation = async(books) => {
+    if (!books) throw "Books is null";
+
+    let formattedBookInfoList = [];
+
+    for (let i = 0; i < books.length; i++) {
+        let bookInfo = {};
+
+        // Checking for the Title
+        if (books[i].volumeInfo.title !== "" || books[i].volumeInfo.title !== undefined) {
+            bookInfo.title = books[i].volumeInfo.title;
+        } else {
+            bookInfo.title = "";
+        }
+
+        // Checking for the Book's Image
+        if ('imageLinks' in books[i].volumeInfo) {
+            bookInfo.imageURL = books[i].volumeInfo.imageLinks.thumbnail;
+        } else {
+            bookInfo.imageURL = "";
+        }
+
+        // Checking for the Author(s)
+        if (books[i].volumeInfo.authors.length > 0) {
+            bookInfo.authors = books[i].volumeInfo.authors.join();
+        } else {
+            bookInfo.authors = "";
+        }
+
+        // Checking for the Price
+        if ('retailPrice' in books[i].saleInfo) {
+            bookInfo.price = books[i].saleInfo.retailPrice.amount;
+        } else {
+            bookInfo.price = 20.00;
+        }
+
+        formattedBookInfoList.push(bookInfo);
+    }
+
+    return formattedBookInfoList;
 };
 
 module.exports = {
