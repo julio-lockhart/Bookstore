a7205441879d01d3531cf5c31e27d1a2fe7b0849
Julio Lockhart <julio.lockhart16@gmail.com>
2017-11-19 13:35:28 -0500

    Added register route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const searchRoute = require('./search');
+const registerRoute = require('./registration');
 
 const constructorMethod = (app) => {
     app.use("/", searchRoute);
+    app.use("/register", registerRoute);
 
     app.use("*", (req, res) => {
         res.redirect("/");
