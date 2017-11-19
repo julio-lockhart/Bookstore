5dd8924af1b941ef240bd8592df8ed8ad2f8f171
Julio Lockhart <julio.lockhart16@gmail.com>
2017-11-19 16:56:29 -0500

    Adding route to show user information

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const searchRoute = require('./search');
 const registerRoute = require('./registration');
+const userRoute = require('./user');
 
 const constructorMethod = (app) => {
     app.use("/", searchRoute);
     app.use("/register", registerRoute);
+    app.use("/user", userRoute);
 
     app.use("*", (req, res) => {
         res.redirect("/");
