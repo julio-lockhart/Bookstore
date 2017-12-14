19ff77017aa09599fee3064bcc13a2b172d06ad9
Julio Lockhart <julio.lockhart16@gmail.com>
2017-12-14 17:51:31 -0500

    Auto stash before merge of "dev" and "origin/dev"

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require('./config');
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("We've now got a server!");
-    console.log("Your routes will be running on http://localhost:3000\n");
+    console.log("Your routes will be running on http://localhost:" + port);
 });
